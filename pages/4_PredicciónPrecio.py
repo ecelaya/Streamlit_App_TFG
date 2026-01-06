eb21a0ad6fa83a794ef53821cfcaf711206242ad
Eloy Celaya Lopez <eloycl2003@gmail.com>
2026-01-06 15:01:27 +0100

    Update 4_PredicciónPrecio.py

--- pages/4_PredicciónPrecio.py
+++ pages/4_PredicciónPrecio.py
@@ -105,7 +105,7 @@ if st.button("🔮 Predecir precio"):
 
     # Crear DataFrame con EXACTAS columnas del modelo
     input_data = pd.DataFrame({
-        "log_surface": [surface],
+        "log_surface": [np.log(surface)],
         "Rooms": [rooms],
         "Bathrooms": [bathrooms],
         "Floor": [floor],
@@ -136,5 +136,6 @@ if st.button("🔮 Predecir precio"):
         "del mercado inmobiliario de Madrid en el año 2023. "
 
         "El resultado tiene carácter orientativo y no constituye una valoración oficial.")
+
 
 
